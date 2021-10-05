e7b0dc3d9b12ff697b6677425c3647aae6d1f469
Alvaro castillo <6666alvaro666@gmail.com>
2021-10-04 22:30:40 -0500

    :art: resposive desing

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,23 +1,37 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Moderator from './components/Moderator';
 import Admin from './components/Admin';
 import LoginForm from './components/LoginForm';
 
+import clienteAxios from './config/axios';
+
+const AppContext = React.createContext();
+
 function App() {
-  const [role, setrole] = useState('admin');
+  const [role, setrole] = useState('moderator');
   const [user, setUser] = useState({});
 
+  useEffect(() => {}, []);
+
   return (
-    <div className="App">
-      <div className="App-container ">
-        <div className="App-content">
-          <Header />
-          {!user ? <LoginForm /> : role === 'admin' ? <Admin /> : <Moderator />}
+    <AppContext.Provider value={{}}>
+      <div className="App">
+        <div className="App-container ">
+          <div className="App-content">
+            <Header />
+            {!user ? (
+              <LoginForm />
+            ) : role === 'admin' ? (
+              <Admin />
+            ) : (
+              <Moderator />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </AppContext.Provider>
   );
 }
 
